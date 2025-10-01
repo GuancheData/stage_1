806766e5faa8cba4fe9e1eaff8d178877c4992ce
Joel <joel.ojeda.santana.05@gmail.com>
2025-10-01 13:06:03 +0100

    added function to store metadata in SQLite

--- indexer/Main.py
+++ indexer/Main.py
@@ -1,5 +1,5 @@
 from indexer.MetadataParser import MetadataParser
 
 if __name__ == "__main__":
-    metadataParser = MetadataParser("datalake", "bookCounter.txt")
+    metadataParser = MetadataParser("../crawler/src/main/python/datalake", "../crawler/src/main/python/bookCounter.txt")
     metadataParser.parseMetadata()
--- indexer/MetadataParser.py
+++ indexer/MetadataParser.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import re
+import sqlite3
 
 class MetadataParser:
 
@@ -7,14 +8,44 @@ class MetadataParser:
         self.booksMetadataContentPath = booksMetadataContentPath
         self.bookCounterPath = bookCounterPath
 
+    def store(self, id=None, title=None, author=None, language=None):
+        if id is None:
+            return "error"
+        with sqlite3.connect("database.db") as connection:
+            cursor = connection.cursor()
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS metadata (
+            id INTEGER PRIMARY KEY,
+            title TEXT NOT NULL,
+            author TEXT,
+            language TEXT
+            );
+            """)
+
+            cursor.execute("""
+            INSERT INTO metadata (id, title, author, language)
+            VALUES (?, ?, ?, ?)
+            """, (id, title, author, language))
+
+
 
     def parseMetadata(self):
         route = Path(self.booksMetadataContentPath)
         files = route.rglob('*header.txt')
-        pattern = re.compile(r"Title:\s*(.+)|Author:\s*(.+)|Language: \s*(.+)")
+        pattern = re.compile(r"Title:\s*(.+)|Author:\s*(.+)|Language:\s*(.+)")
         for f in files:
             with f.open('r') as file:
+                metadata = {}
+                filename = f.name
+                fileMatch = re.search(r"^(\d+)_", filename)
                 for line in file:
                     match = pattern.search(line)
                     if match:
-                        print(match)
+                        matchSplit = match.group(0).split(sep=":", maxsplit=1)
+                        metadata[matchSplit[0]] = matchSplit[1]
+                        print(f"found a match in {fileMatch.group(1)}")
+                self.store(
+                    id=int(fileMatch.group(1)),
+                    title=metadata["Title"] if "Title" in metadata.keys() else None,
+                    author=metadata["Author"] if "Author" in metadata.keys() else None,
+                    language=metadata["Language"] if "Language" in metadata.keys() else None)
