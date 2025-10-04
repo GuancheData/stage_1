c8cd2d9090ffb9e55d53f6a72cc46cda7cdc9b01
Joel <joel.ojeda.santana.05@gmail.com>
2025-10-04 18:51:07 +0100

    Finished crawler feature implementation

--- control/Main.py
+++ control/Main.py
@@ -1,5 +1,5 @@
 from control.PipelineController import PipelineController
 
 if __name__ == '__main__':
-    controller = PipelineController()
-    controller.pipeline(105)
+    controller = PipelineController(total_books=135)
+    controller.pipeline(books_to_download=20)
--- control/PipelineController.py
+++ control/PipelineController.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import random
 
 from crawler.src.main.python.Crawler import Crawler
 
@@ -8,24 +7,33 @@ class PipelineController:
     def __init__(self, control_path = Path("control"), total_books = 1000):
         self.total_books = total_books
         self.control_path = control_path
-        self.download_path = control_path / "downloaded_books.txt"
+        self.downloaded_path = control_path / "downloaded_books.txt"
+        self.failed_to_download_path = control_path / "failed_to_download_books.txt"
         self.index_path = control_path / "indexed_books.txt"
         self.crawler = Crawler(datalakeStructure="id")
+        self.not_downloaded = set(range(1, self.total_books + 1)) - self.downloaded()
 
     def downloaded(self):
-        return set(self.download_path.read_text().splitlines()) if self.download_path.exists() else set()
+        return set((int(x) for x in set(self.downloaded_path.read_text().splitlines()))) if self.downloaded_path.exists() else set()
+
+    def failed_to_download(self):
+        return set((int(x) for x in set(self.failed_to_download_path.read_text().splitlines()))) if self.failed_to_download_path.exists() else set()
 
     def indexed(self):
-        return set(self.index_path.read_text().splitlines()) if self.index_path.exists() else set()
+        return set((int(x) for x in set(self.index_path.read_text().splitlines()))) if self.index_path.exists() else set()
 
     def pipeline(self, books_to_download = 1):
         self.control_path.mkdir(parents=True, exist_ok=True)
-        ready_to_index = self.downloaded() - self.indexed()
-        # ready_to_index = False
+        # ready_to_index = self.downloaded() - self.indexed()
+        ready_to_index = False
         if ready_to_index:
             self.index(ready_to_index)
         for _ in range(books_to_download):
-            self.download()
+            if not self.download():
+                break
+        self.not_downloaded = set(range(1, self.total_books + 1)) - self.downloaded()
+        if self.failed_to_download():
+            print("[DOWNLOAD] These books could not be downloaded:", sorted(list(self.failed_to_download())))
 
     def index(self, ready_to_index):
         for book_id in ready_to_index:
@@ -36,10 +44,22 @@ class PipelineController:
             print(f"[INDEX] Book {book_id} successfully indexed.")
 
     def download(self):
-        candidate_id = str(random.randint(1, self.total_books))
-        if candidate_id not in self.downloaded():
-            print(f"[DOWNLOAD] Downloading new book with ID {candidate_id}...")
-            was_successful = self.crawler.crawlBook(candidate_id)
-            if was_successful:
-                with open(self.download_path, "a", encoding="utf-8") as f:
-                    f.write(f"{candidate_id}\n")
+        if self.not_downloaded:
+            candidate_id = self.not_downloaded.pop()
+            while candidate_id in self.failed_to_download():
+                if self.not_downloaded:
+                    candidate_id = self.not_downloaded.pop()
+                else:
+                    return True
+        else:
+            print("[DOWNLOAD] There are no more books to download")
+            return False
+        print(f"[DOWNLOAD] Downloading new book with ID {candidate_id}...")
+        was_successful = self.crawler.crawlBook(candidate_id)
+        if was_successful:
+            with open(self.downloaded_path, "a", encoding="utf-8") as f:
+                f.write(f"{candidate_id}\n")
+        else:
+            with open(self.failed_to_download_path, "a", encoding="utf-8") as f:
+                f.write(f"{candidate_id}\n")
+        return True
