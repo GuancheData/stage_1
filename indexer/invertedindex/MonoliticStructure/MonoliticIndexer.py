08892a0c8b686ba4525ce480c50fc1fffbf8ecca
Pablo <pabcablan2005@gmail.com>
2025-10-04 23:47:20 +0100

    MonoliticIndexer Structure Merged

---
+++ indexer/invertedindex/MonoliticStructure/MonoliticIndexer.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+class MonoliticIndexer:
+    def __init__(self, output_json_path):
+        self.output_json_path = output_json_path
+        self.inverted_index = {}
+
+    def insertInformation(self, bookId, positionDict):
+        self.bookId = bookId
+        self.positionDict = positionDict
+
+        for word, positions in self.positionDict.items():
+            if word not in self.inverted_index:
+                self.inverted_index[word] = {}
+            self.inverted_index[word][bookId] = {
+                'positions': positions,
+                'frequency': len(positions)
+            }
+
+    def _save(self):
+        if os.path.exists(self.output_json_path):
+            with open(self.output_json_path, 'r') as file:
+                try:
+                    existing_index = json.load(file)
+                except json.JSONDecodeError:
+                    existing_index = {}
+        else:
+            existing_index = {}
+        existing_index.update(self.inverted_index)
+        with open(self.output_json_path, 'w') as file:
+            if existing_index:
+                json.dump(existing_index, file, indent=2)
+                print(f"Index saved in {self.output_json_path}\n")
