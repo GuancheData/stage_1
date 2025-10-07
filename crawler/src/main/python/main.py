e704fbb1b741fe9d676eca74740595a9f52917fe
Joel <joel.ojeda.santana.05@gmail.com>
2025-10-07 23:48:57 +0100

    datalake and test update

--- crawler/src/main/python/main.py
+++ crawler/src/main/python/main.py
@@ -4,5 +4,5 @@ import sys
 if __name__ == "__main__":
     datalake_path = sys.argv[1]
     logs_output_path = sys.argv[2]
-    crawler = CrawlerController(datalake_path, logs_output_path, datalake_structure="id")
-    crawler.download(15)
+    crawler = CrawlerController(datalake_path, logs_output_path, datalake_structure="date")
+    crawler.download(10)
